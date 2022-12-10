e72bd4ea738d8b9dfac5a70893d23a48c05e761f
Tatiana Kramarenko <tanya19101996@gmail.com>
2022-12-10 15:22:09 +0200

    task-2_done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+// Напиши скрипт, который для каждого элемента массива ingredients:
+
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
+
+function createIngridientListEl(name) {
+  const liEl = document.createElement('li');
+  liEl.textContent = name;
+  liEl.classList.add('item');
+  return liEl;
+}
+
+const ingridientsList = [];
+ingredients.forEach(elem => ingridientsList.push(createIngridientListEl(elem)));
+
+
+
+const ulEl = document.querySelector('#ingredients');
+ulEl.append(...ingridientsList);
