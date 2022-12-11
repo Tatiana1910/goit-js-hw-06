557bbd6182f3ba644bfe565c02cb90452198ab6f
Tatiana Kramarenko <tanya19101996@gmail.com>
2022-12-11 18:10:35 +0200

    task-9_done

--- js/task-09.js
+++ js/task-09.js
@@ -1,4 +1,6 @@
-// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
+// // Напиши скрипт, который изменяет цвета фона элемента <body> через
+// инлайн стиль при клике на button.change-color и выводит значение цвета в
+// span.color.
 
 // <div class="widget">
 //   <p>Background color: <span class="color">-</span></p>
@@ -16,3 +18,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtnEl = document.querySelector('.change-color');
+const colorNameEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+changeColorBtnEl.addEventListener('click', () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorNameEl.textContent = `${bodyEl.style.backgroundColor}`;
+});
