8ca7eb365bdae41376a9f711e11a4dfbbf05bf9b
Tatiana Kramarenko <tanya19101996@gmail.com>
2022-12-11 18:10:25 +0200

    task-2_done

--- js/task-02.js
+++ js/task-02.js
@@ -7,16 +7,30 @@ const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Con
 // Добавит элементу класс item.
 // После чего вставит все <li> за одну операцию в список ul#ingredients.
 
-const list = document.querySelector('#ingredients');
-const ingridientsList = [];
+// const list = document.querySelector('#ingredients');
+// const ingridientsList = [];
 
-ingredients.forEach(ingredient => {
-  const liEl = document.createElement('li');
-  liEl.className = 'item';
-  liEl.textContent = ingredient;
-  ingridientsList.push(liEl);
+// ingredients.forEach(ingredient => {
+//   const liEl = document.createElement('li');
+//   liEl.className = 'item';
+//   liEl.textContent = ingredient;
+//   ingridientsList.push(liEl);
+// });
+
+// list.append(...ingridientsList);
+
+// console.log(list);
+
+// 2 вариант
+const list = document.querySelector('#ingredients');
+
+const ingridientsList = ingredients.map(el => {
+  let item = document.createElement('li');
+  item.className = 'item';
+  item.textContent = el;
+
+  return item;
 });
 
 list.append(...ingridientsList);
-
 console.log(list);
